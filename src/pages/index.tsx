c69d5a136d9b5129b6cb582bcb0c4dbaf8c4fa46
Allan Victor Batista <50140443+allan-batistaa@users.noreply.github.com>
2022-09-06 15:39:51 -0300

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
+import SecondComponent from '../components/ second-component/ Second-component'
+import FirstComponent from '../components/first-component/First-component'
 
 const Home: NextPage = () => {
 
   return (
     <>
-    <Head>
-      <title>Nome da sua empresa/marca</title>
-    </Head>
-    <div>teste</div>
+      <Head>
+        <title>Nome da sua empresa/marca</title>
+      </Head>
+      <div>HOME</div>
+      <div><FirstComponent/></div>
+      <div><SecondComponent/></div>
     </>
   )
 }
