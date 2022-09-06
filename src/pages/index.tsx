415e9a1a92d52ec2955d6f9f7504d6b0a843944d
allan-batistaa <allanbatista2010@gmail.com>
2022-09-06 14:42:37 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+    <Head>
+      <title>Nome da sua empresa/marca</title>
+    </Head>
+    <div>teste</div>
+    </>
+  )
+}
+
+export default Home
